89b1cf2cd3b44cf3b9ff520296ee5bb5c0b0a804
KaberTP <thomaskbr@gmail.com>
2020-09-10 16:35:11 +0200

    changes made

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.post('/',(req,res)=> {
         });
   })
   if (process.env.NODE_ENV === "production"){
-    app.use(express.static("frontend/build"));
+    app.use(express.static("itunes-api-app/build"));
   
     app.get('*',(req,res) =>{
-  res.sendFile(path.resolve(__dirname, "itunes-api-app", "build", "index.html"));
-  })
-} 
+      res.sendFile(path.resolve(__dirname, "itunes-api-app", "build", "index.html"));
+    })
+  } 
 
 //IN PRODUCTION 
 
